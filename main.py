810dada2ca2ae70c1c2a5e6a4b87608364d90852
eniolaomotee <eniolaomotomi@gmail.com>
2025-02-20 21:02:31 +0100

    Added CORS for telex

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import json
 import feedparser
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"], 
+)
+
 
 RSS_FEED_URL = "https://status.flutterwave.com/history.rss"
 
